3a63d7d93ceb8ade2cbf8e3e0e783f661bc448b2
skyewong <skyewong.personal@gmail.com>
2023-04-23 14:12:55 +0800

    Users can now view the entire YT video description

--- views/scraping_views.py
+++ views/scraping_views.py
@@ -284,6 +284,16 @@ class VideoView(BaseView):
         self.disable_buttons()
         embed = self.get_embed()
         await interaction.response.edit_message(view=self, embed=embed)
+        
+    @button(emoji="📃", style=ButtonStyle.grey, custom_id="description")
+    async def show_description(self, button: Button, interaction: Interaction):
+        embed = Embed()
+        video = self.videos[self.page]
+
+        embed.set_author(name=video.title)
+        embed.set_thumbnail(url=video.thumbnail_url)
+        embed.description = video.description[:4096]  # upper limit for description length is 4096.
+        await interaction.send(embed=embed, ephemeral=True)
 
     @button(emoji="▶️", style=ButtonStyle.blurple, custom_id="next")
     async def next(self, button: Button, interaction: Interaction):
