82b45234e43705b2f8ef46364a80ca355d1a3720
skyewong <skyewong.personal@gmail.com>
2023-04-25 10:12:44 +0800

    Attempted to make village

---
+++ village/generate_path.py
@@ -0,0 +1,75 @@
+import random
+from PIL import Image, ImageDraw
+
+# set the background image
+background_image = Image.open("resources/village_bg.png")
+
+# calculate the grid size based on the size of the image
+grid_size = min(background_image.width, background_image.height) // 10
+house_size = int(grid_size * 0.8)
+
+# calculate the number of houses that can fit in the grid
+num_houses_w = int(background_image.width / grid_size)
+num_houses_h = int(background_image.height / grid_size)
+num_houses = num_houses_w * num_houses_h
+
+# load the house image
+house_image = Image.open("resources/house.png")
+
+# create a list of house positions
+house_positions = []
+for y in range(num_houses_h):
+    for x in range(num_houses_w):
+        offset_x = random.randint(-20, 20)
+        offset_y = random.randint(-20, 20)
+        house_x = x * grid_size + offset_x
+        house_y = y * grid_size + offset_y
+        house_positions.append((house_x, house_y))
+
+# check for overlapping houses
+for i in range(num_houses):
+    for j in range(i+1, num_houses):
+        x1, y1 = house_positions[i]
+        x2, y2 = house_positions[j]
+        if abs(x1 - x2) < house_size and abs(y1 - y2) < house_size:
+            offset_x = random.randint(-20, 20)
+            offset_y = random.randint(-20, 20)
+            house_positions[j] = (x2 + offset_x, y2 + offset_y)
+
+# create a drawing context
+draw = ImageDraw.Draw(background_image)
+
+# draw the houses on the image
+for pos in house_positions:
+    x, y = pos
+    x -= house_size // 2
+    y -= house_size // 2
+    background_image.paste(house_image, (x, y), house_image)
+
+# create a list of paths
+paths = []
+for i in range(num_houses):
+    for j in range(i+1, num_houses):
+        x1, y1 = house_positions[i]
+        x2, y2 = house_positions[j]
+        dx = x2 - x1
+        dy = y2 - y1
+        if dx != 0 and dy != 0:
+            continue
+        path = [(x1, y1)]
+        if dx != 0:
+            x3 = x2
+            y3 = y1
+        else:
+            x3 = x1
+            y3 = y2
+        path.append((x3, y3))
+        path.append((x2, y2))
+        paths.append(path)
+
+# draw the paths on the image
+for path in paths:
+    draw.line(path, fill="black", width=3)
+
+# save the image
+background_image.show()
---
+++ village/generate_village_img.py
@@ -0,0 +1,84 @@
+from PIL import Image, ImageDraw
+import random
+import sys
+
+# Create the background image
+background = Image.open("resources/village_bg.png")
+
+# Define the size of the background image and the size of each grid cell
+width, height = background.size
+cell_width, cell_height = 110, 80
+
+# Define the starting position of the grids
+start_x, start_y = 70, 60
+
+# Define the ending position of the grids
+end_x, end_y = width - 60, height - 20
+
+# Calculate the number of rows and columns in the grid
+num_rows = (end_y - start_y) // cell_height
+num_cols = (end_x - start_x) // cell_width
+
+# Define the list of house images
+house_images = [
+    Image.open("resources/house1.png"),
+    Image.open("resources/house2.png"),
+]
+for i in house_images:
+    i.thumbnail((100, 100))
+
+# Define the list of used grid cells
+used_cells = []
+
+# Define the list of houses
+houses = []
+
+# Generate a random village map
+for i in range(num_cols * num_rows - random.randint(1, 5)):
+    # Find a random unused grid cell
+    while True:
+        if len(used_cells) == num_cols * num_rows:
+            print("There are more houses than the grids! They could not be generated.")
+            sys.exit(0)
+            
+        row = random.randint(0, num_rows - 1)
+        col = random.randint(0, num_cols - 1)
+        if (row, col) not in used_cells:
+            used_cells.append((row, col))
+            break
+
+    # Choose a random house image
+    house_image = random.choice(house_images)
+
+    # Calculate the coordinates to paste the house image with an offset based on the starting position
+    offset_x = cell_width / 10
+    offset_y = cell_height / 10
+    x = start_x + col * cell_width + random.randint(-offset_x, offset_x)
+    y = start_y + row * cell_height + random.randint(-offset_y, offset_y)
+
+    # Check if the house is within the allowed bounds
+    if x > end_x - house_image.width or y > end_y - house_image.height:
+        continue
+
+    # Add the house to the list of houses
+    houses.append({'image': house_image, 'x': x, 'y': y})
+
+# Sort the houses by their y coordinate to ensure they are displayed in the correct order
+houses = sorted(houses, key=lambda h: h['y'])
+
+# Draw the grid lines onto the background
+draw = ImageDraw.Draw(background)
+for row in range(num_rows + 1):
+    y = start_y + row * cell_height
+    for col in range(num_cols + 1):
+        x = start_x + col * cell_width
+        draw.rectangle((x, y, x + cell_width, y + cell_height), outline=(255, 0, 0))
+
+# Paste the house images onto the background
+for house in houses:
+    # Paste the house image onto the background
+    background.paste(house['image'], (house['x'], house['y']), house["image"])
+
+# Save the final image
+background.save('village_map.png')
+input()
