5b22a8b93a6efa61e47da07b9cd40bd4dc6cb883
skyewong <skyewong.personal@gmail.com>
2023-04-30 23:13:50 +0800

    added /exchange

--- cogs/currency.py
+++ cogs/currency.py
@@ -11,11 +11,16 @@ from cooldowns import SlashBucket
 from utils.postgres_db import Database
 
 # my modules and constants
+from utils import functions, constants
 from utils.player import Player
+from utils.functions import TextEmbed
 from utils.constants import SCRAP_METAL, COPPER
 from views.template_views import ConfirmView
 
 # default modules
+import random
+from typing import Literal
+import operator
 
 
 class Currency(commands.Cog, name="Resource Reserve"):
@@ -246,6 +251,96 @@ class Currency(commands.Cog, name="Resource Reserve"):
         embed = self.get_sell_item_embed((item,), total_price)
 
         await interaction.send(f"{interaction.user.mention}, you successfully sold the items!", embed=embed)
+
+    @nextcord.slash_command(name="exchange")
+    async def exchange_currency_cmd(self, interaction: Interaction):
+        """Exchange your currency between scrap metals and coppers."""
+        pass
+
+    async def exchange_currencies(
+        self,
+        interaction: Interaction,
+        from_currency: Literal["scrap_metal", "copper"],
+        to_currency: Literal["scrap_metal", "copper"],
+        amount: str,
+    ):
+        """Convert from one currency to another."""
+        if from_currency not in ("scrap_metal", "copper") or to_currency not in ("scrap_metal", "copper"):
+            raise ValueError("Currency must be either `scrap_metal` or `copper`.")
+        
+        try:
+            amount = functions.text_to_num(amount)
+        except functions.TextToNumException:
+            await interaction.send(embed=TextEmbed("The amount is invalid."))
+            return
+        
+        if from_currency == "scrap_metal":
+            exchange_rate = 25 + random.uniform(0, +2.5)
+            op = operator.truediv
+        elif from_currency == "copper":
+            exchange_rate = 25 + random.uniform(-2.5, 0)
+            op = operator.mul
+            
+        exchange_rate = round(exchange_rate, 2)
+        exchanged_amount = round(op(amount, exchange_rate))
+        
+        db: Database = interaction.client.db
+        player = Player(db, interaction.user)
+
+        async with db.pool.acquire() as conn:
+            async with conn.transaction():
+                from_amount = await player.modify_currency(from_currency, -amount)
+                to_amount = await player.modify_currency(to_currency, exchanged_amount)
+                
+        from_currency_msg = from_currency.replace('_', ' ')
+        to_currency_msg = to_currency.replace('_', ' ')
+        
+        embed = Embed()
+        embed.description = f"The current exchange rate is **{exchange_rate}** {to_currency_msg} for 1 {from_currency_msg}.\n" \
+                            f"You got **{exchanged_amount} {constants.CURRENCY_EMOJIS[to_currency]}**."
+                            
+        embed.add_field(
+            name="Current Balance",
+            value=f"{from_currency_msg.title()}: {constants.CURRENCY_EMOJIS[from_currency]} `{from_amount:,}`\n"
+                  f"{to_currency_msg.title()}: {constants.CURRENCY_EMOJIS[to_currency]} `{to_amount:,}`\n"
+        )
+        await interaction.send(embed=embed)
+
+    @exchange_currency_cmd.subcommand(name="to-copper")
+    async def exchange_to_copper(
+        self, 
+        interaction: Interaction,
+        scrap_metal: str=SlashOption(
+            name="scrap-metal",
+            description="Amount of scrap metal to exchange"
+        )
+    ):
+        """Convert your scrap metals to coppers."""
+        
+        await self.exchange_currencies(
+            interaction,
+            "scrap_metal",
+            "copper",
+            scrap_metal,
+        )
+
+    @exchange_currency_cmd.subcommand(name="to-scrap")
+    async def exchange_to_scrap(
+        self, 
+        interaction: Interaction,
+        copper: str=SlashOption(
+            name="copper",
+            description="Amount of copper to exchange"
+        )
+    ):
+        """Convert your coppers to scrap metals."""
+        
+        await self.exchange_currencies(
+            interaction,
+            "copper",
+            "scrap_metal",
+            copper,
+        )
 
 
 def setup(bot: commands.Bot):
