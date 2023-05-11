a94565847054329249d03492015a0f385d07dd32
skyewong <skyewong.personal@gmail.com>
2023-05-11 22:00:59 +0800

    Added more pages to /guide

--- views/utility_views.py
+++ views/utility_views.py
@@ -33,7 +33,9 @@ class HelpView(BaseView):
         self.old_selected_values = ["All"]
 
     def _get_cogs_option(self) -> list[SelectOption]:
-        options: list[SelectOption] = [SelectOption(label="All", emoji="🌐", default=True)]
+        options: list[SelectOption] = [
+            SelectOption(label="All", emoji="🌐", default=True)
+        ]
         for cog_name in self.mapping:
             cog = self.mapping[cog_name][0]
             emoji = getattr(cog, "COG_EMOJI", None)
@@ -54,7 +56,10 @@ class HelpView(BaseView):
         if description:
             embed.description = description
         if set_author:
-            avatar = self.interaction.client.user.avatar or self.interaction.client.user.default_avatar
+            avatar = (
+                self.interaction.client.user.avatar
+                or self.interaction.client.user.default_avatar
+            )
             embed.set_author(name=author_name, icon_url=avatar.url)
         if not command_list:
             for cog_name in self.mapping:
@@ -80,14 +85,18 @@ class HelpView(BaseView):
                         cmd_in_guild = True
             if cmd_in_guild:
                 filtered.append(i)
-        final_cmd_list = filtered[self.get_page_start_index() : self.get_page_end_index() + 1]
+        final_cmd_list = filtered[
+            self.get_page_start_index() : self.get_page_end_index() + 1
+        ]
         for cmd in final_cmd_list:
             value = cmd.description if cmd.description else "..."
             name = f"</{cmd.qualified_name}:{list(cmd.command_ids.values())[0]}>"
             if len(cmd.children) > 0:
                 name += " `has subcommands`"
             embed.add_field(name=name, value=f"`➸` {value}", inline=False)
-        embed.set_footer(text=f"Page {self.page}/{math.ceil(len(self.cmd_list) / self.cmd_per_page)}")
+        embed.set_footer(
+            text=f"Page {self.page}/{math.ceil(len(self.cmd_list) / self.cmd_per_page)}"
+        )
         return embed
 
     @select(
@@ -153,13 +162,17 @@ class HelpView(BaseView):
         embed = self.help_embed()
         await interaction.response.edit_message(embed=embed, view=self)
 
-    @button(emoji="⏮️", style=nextcord.ButtonStyle.blurple, custom_id="first", disabled=True)
+    @button(
+        emoji="⏮️", style=nextcord.ButtonStyle.blurple, custom_id="first", disabled=True
+    )
     async def first(self, button: Button, interaction: Interaction):
         self.page = 1
         self.btn_disable()
         await self.get_embed_and_send_msg(interaction)
 
-    @button(emoji="◀️", style=nextcord.ButtonStyle.blurple, disabled=True, custom_id="back")
+    @button(
+        emoji="◀️", style=nextcord.ButtonStyle.blurple, disabled=True, custom_id="back"
+    )
     async def back(self, button: Button, interaction: Interaction):
         self.page -= 1
         self.btn_disable()
@@ -204,20 +217,21 @@ class GuideView(BaseView):
     pages: list[GuidePage] = [
         GuidePage(
             title="Introduction",
-            description="Welcome to BOSS, the Discord bot for a post-apocalyptic wasteland. \n"
+            description="Welcome to BOSS, the Discord bot for a post-apocalyptic wasteland following World War III. \n"
             "Scavenge for resources, complete missions, and participate in events to earn valuable currency. \n\n"
             "With BOSS's help, you can navigate this harsh world and build your wealth. "
-            "So join us and let BOSS be your guide to survival and prosperity!",
+            "So join us and let BOSS be your guide to survival and prosperity! \n\n"
+            "To start playing BOSS, use </help:964753444164501505> to view a list of available commands and get started with earning and managing their currency.",
         ),
         GuidePage(
             title="Currency System",
-            description="The currency system includes multiple types of currency, each with its own value and uses.",
+            description="In the world of BOSS, there are two main types of currency: scrap metal and copper.",
             fields=[
                 EmbedField(
                     "Scrap Metal",
                     "Scrap metal is the basic currency in BOSS, used for everyday transactions. \n"
-                    "It's easy to find and earn, but has a relatively low value compared to other types of currency."
-                    "Users need to manage their scrap metal wisely to build their wealth and survive.",
+                    "It's easy to find and earn, but has a relatively low value compared to other types of currency. "
+                    "Users need to manage their scrap metal wisely to build their wealth and survive. ",
                     False,
                 ),
                 EmbedField(
@@ -225,10 +239,36 @@ class GuideView(BaseView):
                     "Copper is a valuable and versatile currency in BOSS, used for creating and repairing weapons, armor, and electronic devices. "
                     "Users can earn copper by scavenging for it or completing tasks and challenges. \n"
                     "As a currency, copper is worth more than basic resources like scrap metal or cloth. "
-                    "It can be traded for valuable resources like ammunition, fuel, or medicine. ",
+                    "It can be traded for valuable resources like ammunition, fuel, or medicine. \n\n"
+                    f"1 copper is worth {constants.COPPER_SCRAP_RATE} scrap metals.",
                     False,
                 ),
             ],
+        ),
+        GuidePage(
+            title="How to survive.",
+            description="The ultimate guide to survive in BOSS.",
+            fields=[
+                EmbedField(
+                    "By scavenging for resources",
+                    "Use </hunt:1079601533215330415>, </dig:1079644728921948230>, </mine:1102561135988838410> and more! "
+                    "Each activity has its own risks and rewards, and users can use the resources they find to build their wealth and purchase goods and services.",
+                    False,
+                ),
+                EmbedField(
+                    "By completing tasks and challenges",
+                    "Users can also earn currency by completing tasks and challenges. "
+                    "These may include delivering goods, defending against raiders, or completing other objectives. "
+                    "Users can use the /missions command to view available missions and track their progress.",
+                    False,
+                ),
+            ],
+        ),
+        GuidePage(
+            title="Manage your wealth",
+            description="Use your currency to purchase goods and services. \n\n"
+            "Use the /shop command to view available items and their prices, and use the /buy command to purchase items. \n"
+            "You can also use the </trade:1102561137893056563> command to trade currency with virtual villagers, and acquire valuable resources to build your wealth. ",
         ),
     ]
 
@@ -237,7 +277,9 @@ class GuideView(BaseView):
         self.current_page = 0
         self.msg: nextcord.WebhookMessage | nextcord.PartialInteractionMessage = None
 
-        choose_page_select = [i for i in self.children if i.custom_id == "choose_page"][0]
+        choose_page_select = [i for i in self.children if i.custom_id == "choose_page"][
+            0
+        ]
         for index, page in enumerate(self.pages):
             choose_page_select.options.append(
                 SelectOption(
@@ -256,7 +298,9 @@ class GuideView(BaseView):
         return self.pages[self.current_page]
 
     def update_view(self):
-        choose_page_select = [i for i in self.children if i.custom_id == "choose_page"][0]
+        choose_page_select = [i for i in self.children if i.custom_id == "choose_page"][
+            0
+        ]
         for option in choose_page_select.options:
             option: SelectOption
             if option.value == self.current_page:
