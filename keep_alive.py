59df892f413c6c369c0f2a201a45db4f2466bbe7
skyewong <skyewong.personal@gmail.com>
2023-05-21 20:22:22 +0800

    updated website appearance (which is https://boss-bot.onrender.com/)

--- keep_alive.py
+++ keep_alive.py
@@ -1,6 +1,16 @@
 from flask import Flask
+import requests
 from threading import Thread
-import markdown
+import json
+
+
+def render_markdown(markdown_text):
+    url = "https://api.github.com/markdown"
+    headers = {"Content-Type": "application/json"}
+    data = {"text": markdown_text, "mode": "markdown"}
+    response = requests.post(url, headers=headers, data=json.dumps(data))
+    return response.text
+
 
 app = Flask("")
 
@@ -8,12 +18,43 @@ app = Flask("")
 @app.route("/")
 def home():
     html = """
-        <h1 style="color: #0071ad; font-family: sans-serif">Boss Discord Bot</h1>
-        <h3 style="font-family: sans-serif"><a href="https://discord.com/api/oauth2/authorize?client_id=906505022441918485&permissions=448824593472&scope=bot">Add me to your server</a></h3>
-        <hr> 
+        <!DOCTYPE html>
+        <html>
+        <head>
+            <meta charset="UTF-8">
+            <title>BOSS Bot</title>
+            <meta name="viewport" content="width=device-width, initial-scale=1">
+            <link rel="icon" type="image/png" href="https://i.imgur.com/3DTqt8K.png">
+            <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/reset-css@5.0.1/reset.min.css">
+            <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/github-markdown-css/5.2.0/github-markdown-dark.css">
+            <style>
+                .markdown-body {
+                    box-sizing: border-box;
+                    min-width: 200px;
+                    max-width: 100vw;
+                    margin: 0 auto;
+                    padding: 45px;
+                }
+
+                @media (max-width: 767px) {
+                    .markdown-body {
+                        padding: 15px;
+                    }
+                }
+            </style>
+        </head>
+        <body>
+        <article class="markdown-body">
     """
     with open("README.md", "r", encoding="utf-8") as f:
-        html += markdown.markdown(f.read(), extensions=["markdown.extensions.tables"])
+        md = "> **This is the README.md file of the bot [Github repo](https://github.com/skyewong/boss). Check the repo for the full code and more.**\n"
+        md += f.read()
+        html += render_markdown(md)
+    html += """
+        </article>
+        </body>
+        </html>
+    """
     return html
 
 
